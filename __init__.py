81af6ad3ef9bda43d2f8a72b73946446077cc58a
Jozsa Mate <jozsamate97@gmail.com>
2021-04-26 17:07:01 +0300

    megvan a dynamic image communication

--- __init__.py
+++ __init__.py
@@ -1,6 +1,24 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, Markup
+import io, base64
 import fraktal_py as fp
-from pylab import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+# generate encoded matplotlib image from fractal parameters
+def generateEncodedFractalImage(fractal_type, fractal_stepsize, fractal_resolution):
+	if fractal_type == "fern":
+		fractal = fp.Fern()
+		fractal.generateFractal(int(fractal_resolution.split("_")[0]), int(fractal_resolution.split("_")[1]), int(fractal_stepsize))
+		mat = np.log(np.asarray(fractal.density_map) + 1.0)
+		fig, ax = plt.subplots(figsize=(15, 8))
+		plt.matshow(mat)
+		img = io.BytesIO()
+		plt.savefig(img, format="png")
+		img.seek(0)
+		plot_url = base64.b64encode(img.getvalue()).decode()
+	else:
+		plot_url = ""
+	return plot_url
 
 app = Flask(__name__)
 
@@ -13,21 +31,13 @@ def home():
 		fractal_type = request.form["type"]
 		fractal_stepsize = request.form["stepsize"]
 		fractal_resolution = request.form["resolution"]
-		if fractal_type == "fern":
-			fractal = fp.Fern()
-			fractal.generateFractal(int(fractal_resolution.split("_")[0]), int(fractal_resolution.split("_")[1]), int(fractal_stepsize))
-			mat = log(asarray(fractal.density_map) + 1.0)
-			matshow(mat)
-			savefig("static/"+fractal_type+".png", dpi=300)
-		return redirect(url_for("fractal", fractal_type=fractal_type))
-	else:
-		return render_template("index.html")
 
-# Add another page.
-@app.route("/<fractal_type>")
-def fractal(fractal_type):
-	# Pass arguments to page 
-	return render_template("fractal.html", fractal_type=fractal_type)
+		# Generate encoded fractal data and html image
+		plot_url = generateEncodedFractalImage(fractal_type, fractal_stepsize, fractal_resolution)
+		plot_fractal = Markup('<img src="data:image/png;base64, {}">'.format(plot_url))
+	else:
+		plot_fractal = ""
+	return render_template("index.html", plot_fractal = plot_fractal)
 
 # Run the server.
 if __name__ == "__main__":
