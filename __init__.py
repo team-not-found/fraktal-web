d57a6e6960134f4ffd3d16bac13f7371fd3abc5e
emil <varadi.emil3@gmail.com>
2021-05-13 21:45:29 +0300

    zoom initial

--- __init__.py
+++ __init__.py
@@ -4,9 +4,55 @@ import fraktal_py as fp
 from matplotlib.figure import Figure
 import numpy as np
 
+
+class Mandelbrot():
+	width = 1920.0
+	height = 1080.0
+	aspect_ratio = width/height
+	x0 = -2.0
+	x1 = 2.0
+	y0 = x0/aspect_ratio
+	y1 = x1/aspect_ratio
+
+	isActive = False
+	zoomFactor = 1.0
+
+	def zoom(x, y):
+		Mandelbrot.zoomFactor *= 2
+
+		xValue = (Mandelbrot.x1 - Mandelbrot.x0)*x/Mandelbrot.width + Mandelbrot.x0
+		yValue = -(Mandelbrot.y1 - Mandelbrot.y0)*y/Mandelbrot.height + Mandelbrot.y1
+
+		newX0 = xValue - 1.0/Mandelbrot.zoomFactor
+		newY0 = yValue - 1.0/Mandelbrot.zoomFactor/Mandelbrot.aspect_ratio
+
+		newX1 = xValue + 1.0/Mandelbrot.zoomFactor
+		newY1 = yValue + 1.0/Mandelbrot.zoomFactor/Mandelbrot.aspect_ratio
+
+		Mandelbrot.x0 = newX0
+		Mandelbrot.x1 = newX1
+		Mandelbrot.y0 = newY0
+		Mandelbrot.y1 = newY1
+
+	def reset():
+		Mandelbrot.width = 1920.0
+		Mandelbrot.height = 1080.0
+		Mandelbrot.aspect_ratio = Mandelbrot.width/Mandelbrot.height
+		Mandelbrot.x0 = -2.0
+		Mandelbrot.x1 = 2.0
+		Mandelbrot.y0 = Mandelbrot.x0/Mandelbrot.aspect_ratio
+		Mandelbrot.y1 = Mandelbrot.x1/Mandelbrot.aspect_ratio
+
+		Mandelbrot.isActive = False
+		Mandelbrot.zoomFactor = 1.0
+
+
+
+
 # generate encoded matplotlib image from fractal parameters
 def generateEncodedFractalImage(fractal_type, fractal_stepsize, fractal_resolution):
 	if fractal_type == "fern":
+		Mandelbrot.reset()
 		fractal = fp.Fern()
 		fractal.generateFractal(int(fractal_resolution.split("_")[0]), int(fractal_resolution.split("_")[1]), int(fractal_stepsize))
 		# Preprocessing (?)
@@ -15,19 +61,45 @@ def generateEncodedFractalImage(fractal_type, fractal_stepsize, fractal_resoluti
 		# Plotting
 		fig = Figure(tight_layout=True)
 		ax = fig.subplots()
-		#ax.matshow(np.random.random((2000, 1000)))
 		ax.matshow(mat)
 		ax.axis("off")
-
 
 		# Encoding
 		buf = io.BytesIO()
 		fig.savefig(buf, format="png", bbox_inches="tight", pad_inches=0)
 		buf.seek(0)
 		plot_url = base64.b64encode(buf.getbuffer()).decode("ascii")
-	else:
-		plot_url = ""
-	return plot_url
+
+		return plot_url
+
+	if fractal_type == "mandelbrot":
+		fractal = fp.Mandelbrot()
+
+		width = Mandelbrot.width
+		height = Mandelbrot.height
+
+		print("Generating mandelbrot ", width, height, Mandelbrot.x0, Mandelbrot.y0, Mandelbrot.x1, Mandelbrot.y1, 1000)
+
+		fractal.generateFractal(int(width), int(height), Mandelbrot.x0, Mandelbrot.y0, Mandelbrot.x1, Mandelbrot.y1, 1000)
+		# Preprocessing (?)
+		mat = np.log(np.asarray(fractal.density_map) + 1.0)
+
+		# Plotting
+		fig = Figure(tight_layout=True)
+		ax = fig.subplots()
+		ax.matshow(mat)
+		ax.axis("off")
+
+		# Encoding
+		buf = io.BytesIO()
+		fig.savefig(buf, format="png", bbox_inches="tight", pad_inches=0)
+		buf.seek(0)
+		plot_url = base64.b64encode(buf.getbuffer()).decode("ascii")
+		Mandelbrot.isActive = True
+
+		return plot_url
+
+	return ""
 
 app = Flask(__name__)
 
@@ -43,7 +115,7 @@ def updateFractal():
 	fractal_stepsize = request.form["stepsize"]
 	fractal_resolution = request.form["resolution"]
 	plot_url = generateEncodedFractalImage(fractal_type, fractal_stepsize, fractal_resolution)
-	plot_fractal = Markup('<img src="data:image/png;base64, {}" id="fractal_image">'.format(plot_url))
+	plot_fractal = Markup('<img src="data:image/png;base64, {}" id="fractal_image" width="1920" height="1080">'.format(plot_url))
 	return jsonify("", render_template("image.html", plot_fractal = plot_fractal))
 
 # Handle zooming
@@ -51,7 +123,13 @@ def updateFractal():
 def zoomInFractal():
 	img_x = request.form["img_x"]
 	img_y = request.form["img_y"]
-	return jsonify("Here you return anything you want")
+
+	if Mandelbrot.isActive: 
+		Mandelbrot.zoom(float(img_x), float(img_y))
+
+	plot_url = generateEncodedFractalImage("mandelbrot", 1000, "1920_1080")
+	plot_fractal = Markup('<img src="data:image/png;base64, {}" id="fractal_image" width="1920" height="1080">'.format(plot_url))
+	return jsonify("", render_template("image.html", plot_fractal = plot_fractal))
 
 # Render home page from external html file.
 @app.route("/")
--- static/index.js
+++ static/index.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {
-  
+$(document).ready(function () {
+
   // On button click we send the form information and generate the fractal and display it
-  $("#generate").click(function(e) {
+  $("#generate").click(function (e) {
     e.preventDefault();
     $.ajax({
       // Send information to 'updateFractal' function
       url: "/update_fractal",
       // Data sent in 'request.form' dict
-      data: {"type":$("#type").val(), "resolution":$("#resolution").val(),"stepsize":$("#stepsize").val()},
+      data: { "type": $("#type").val(), "resolution": $("#resolution").val(), "stepsize": $("#stepsize").val() },
       type: "POST",
       // What it waits back
       dataType: "json",
       // Handle received data
-      success: function(response) {
+      success: function (response) {
         $(fractal_holder).replaceWith(response);
       }
     });
   });
 
   // On click on the image send coordinates for zooming
-  $(document).bind("click", function(){
-    $("#fractal_image").bind("click", function(ev){
+  $(document).bind("click", function () {
+    $("#fractal_image").bind("click", function (ev) {
       // To not fire multiple POST requests
       $(this).off("click");
 
@@ -32,10 +32,10 @@ $(document).ready(function() {
       // Send data and handle response
       $.post({
         url: "/zoom",
-        data: {"img_x":relativeX, "img_y":relativeY},
-        success: function(response){
-          // Handle response
-        alert("word");
+        data: { "img_x": relativeX, "img_y": relativeY },
+        // Handle response
+        success: function (response) {
+          $(fractal_holder).replaceWith(response)
         }
       });
     });
